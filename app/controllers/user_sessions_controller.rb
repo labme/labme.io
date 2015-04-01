a7c7ec4006d63042f1e29173b4aaa1320a9a38a8
Raj Singh <raj@foreversingh.com>
2015-04-01 00:37:54 -0500

    Adding scaffolding per Sorcery to user sessioning
    I do not have authentication fully working for a user that exists. However, the basics are here. Will need to examine documentation that talks about before_do and after_do to control access to certain spaces of the site, then take this to Shiraz for decisions.

---
+++ app/controllers/user_sessions_controller.rb
@@ -0,0 +1,19 @@
+class UserSessionsController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    if @user = login(params[:email], params[:password])
+      redirect_or_back_to(:users, notice: 'Login Successful!')
+    else
+      flash.now[:alert] = 'Login Failed'
+      render action: 'new'
+    end
+  end
+
+  def destroy
+    logout
+    redirect_to (:users, notice: 'Logged out!')
+  end
+end
--- config/initializers/sorcery.rb
+++ config/initializers/sorcery.rb
@@ -176,7 +176,7 @@ Rails.application.config.sorcery.configure do |config|
     # specify username attributes, for example: [:username, :email].
     # Default: `[:email]`
     #
-    # user.username_attribute_names =
+    user.username_attribute_names = [:email]
 
 
     # change *virtual* password attribute, the one which is used until an encrypted one is generated.
