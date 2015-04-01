4aabc476989c7849df59d3880775d5d44a23c95a
Raj Singh <raj@foreversingh.com>
2015-04-01 00:35:18 -0500

    created scaffolding for user model
    during the creation of the scaffolding there were instances where salt and crypted_password were being
    presented to the user - this was removed for security.  Additionally would like to have some validation on the remove of :salt and :crypted_password from the JSON files.  I'm not exactly clear what function those files serve. Finally, added :password and :encrypted_password to UsersController private method to allow comparison between the two to occur during User creation.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ActiveRecord::Base
   authenticates_with_sorcery!
+
+  validates :password, length: { minimum: 7 }
+  validates :password, confirmation: true
+  validates :password_confirmation, presence: true
+
+  validates :email, uniqueness: true
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'user_sessions/new'
+
+  get 'user_sessions/create'
+
+  get 'user_sessions/destroy'
+
+  resources :users
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
