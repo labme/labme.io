f1ff38f701abce3dd04f7c830abeb50cb035e46a
Raj Singh <raj@foreversingh.com>
2015-04-07 15:13:28 -0500

    adding labs controller and views

---
+++ app/controllers/labs_controller.rb
@@ -0,0 +1,17 @@
+class LabsController < ApplicationController
+  def index
+    @labs = Lab.all
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
