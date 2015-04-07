af4622b7ccdb875eb23e3fe73c3980eb1641e426
Raj Singh <raj@foreversingh.com>
2015-04-07 15:15:04 -0500

    adding projects controller and corresponding views

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,17 @@
+class ProjectsController < ApplicationController
+  def index
+    @projects = Project.all
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
