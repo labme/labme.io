030feb29bb1ef8e5e8aa0730b841fcd0e7a2e770
Raj Singh <raj@foreversingh.com>
2015-04-07 11:12:33 -0500

    adding validations and associations to lab

--- app/models/lab.rb
+++ app/models/lab.rb
@@ -1,2 +1,10 @@
 class Lab < ActiveRecord::Base
+  belongs_to :builder, class_name: 'User'
+  belongs_to :assignment
+  has_many :bookings
+  has_many :projects, through: :assignments
+
+  validates_presence_of :name
+  
+  
 end
